f1573779298aab101613fa2d4764b2fc05be9bd8
paravena <adla_paravena@contractor.indeed.com>
2025-06-22 11:37:03 -0400

    extracting render function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,25 +1,19 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-
-  @templates_path Path.expand("../../templates", __DIR__)
-
-  defp render(conv, template, bindings \\ []) do
-    content = @templates_path |> Path.join(template) |> EEx.eval_file(bindings)
-    %{conv | status: 200, resp_body: content}
-  end
+  alias Servy.View
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", bears: bears)
+    View.render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    render(conv, "show.eex", bear: bear)
+    View.render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
